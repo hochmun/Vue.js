8e35fa2035c8d132c6661fd57019ce41e78cdca8
hochmun <s01099760587@gmail.com>
2023-04-03 18:02:39 +0900

    2023/04/03 // 심규영 // Vue.js 챕터1, 챕터2 추가

---
+++ Ch01/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>Ch01. Vue 소개</h3>
+  <p>Hello World</p>
+</template>
+
+<script>
+/**
+ * 날짜 : 2023/04/03
+ * 이름 : 심규영
+ * 내용 : Ch01. Vue.js 소개
+ * 
+ * 개발환경 설정
+ *  - node.js 설치
+ *  - VScode 설치 및 확장 플러그인 (volar, vue3 snippets, prettier, ESLint)
+ *  - Vue.js devtools Chrome 확장 플러그인 설치
+ * 
+ * 프로젝트 생성/실행
+ *  - npm init vite-app 프로젝트 이름
+ *  - cd 프로젝트 이름
+ *  - npm install
+ *  - npm run dev
+ * 
+ * 프로젝트 빌드
+ *  - npm run build
+ */
+export default {
+  name: 'App',
+};
+</script>
