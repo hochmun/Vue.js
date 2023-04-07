ba5a782f80509ed5076184d1d91bb6f8858619d3
hochmun <s01099760587@gmail.com>
2023-04-07 16:47:27 +0900

    2023/04/07 // 심규영 // ch04, ch05 add

---
+++ Ch05/src/App.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <h3>Ch05.Router 실습</h3>
+
+  <h4>1)Router 기본</h4>
+  <router-link to="/hello">hello</router-link> /
+  <router-link to="/welcome">welcome</router-link> /
+  <router-link to="/greeting">greeting</router-link>
+
+  <router-view></router-view>
+  <hr />
+
+  <h4>1)Nested Router</h4>
+  <hr />
+
+  <h4>1)Named view</h4>
+  <hr />
+</template>
+
+<style scoped></style>
