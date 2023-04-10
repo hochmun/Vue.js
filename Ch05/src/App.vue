e607a00bb39a2e51fe574993e49e0a2e52042c51
hochmun <s01099760587@gmail.com>
2023-04-10 19:50:34 +0900

    2023/04/10 // 심규영 // Ch05

--- Ch05/src/App.vue
+++ Ch05/src/App.vue
@@ -1,5 +1,3 @@
-<script setup></script>
-
 <template>
   <h3>Ch05.Router 실습</h3>
 
@@ -7,15 +5,53 @@
   <router-link to="/hello">hello</router-link> /
   <router-link to="/welcome">welcome</router-link> /
   <router-link to="/greeting">greeting</router-link>
+  <hr />
 
+  <h4>2)Nested Router</h4>
+  <router-link to="/parent">Parent</router-link>
+  <hr />
+
+  <h4>3)Named view</h4>
+  <router-link to="/main1">main1</router-link> /
+  <router-link to="/main2">main2</router-link> /
+  <router-link to="/main3">main3</router-link>
+  <hr />
+
+  <h4>4)Router 데이터 전송</h4>
+  <router-link
+    v-bind:to="{
+      name: 'Query',
+      query: {
+        uid: 'a101',
+        name: '홍길동',
+        age: 21,
+      },
+    }"
+    >Query</router-link
+  >
+  /
+  <router-link
+    v-bind:to="{
+      name: 'Param',
+      params: {
+        uid: 'a101',
+        name: '홍길동',
+        age: 21,
+      },
+    }"
+    >Param</router-link
+  >
+  /
+  <router-link to="/form1">Form1</router-link> /
+  <router-link to="/form2">Form2</router-link>
+  <hr />
+
+  <h4>라우팅 컴포넌트 출력</h4>
+  <router-view name="header"></router-view>
   <router-view></router-view>
-  <hr />
-
-  <h4>1)Nested Router</h4>
-  <hr />
-
-  <h4>1)Named view</h4>
-  <hr />
+  <router-view name="footer"></router-view>
 </template>
+
+<script setup></script>
 
 <style scoped></style>
